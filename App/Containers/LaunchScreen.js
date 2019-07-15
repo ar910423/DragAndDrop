99893185d0d3ac072d1fb017f2fbe73463a6546e
ABC <abc@ABCs-Mac.local>
2019-07-15 11:40:30 -0400

    one holder

---
+++ App/Containers/LaunchScreen.js
@@ -0,0 +1,205 @@
+import React, { Component } from 'react'
+import { Text, View, LayoutAnimation } from "react-native"
+import _ from "lodash";
+import { DragContainer, Draggable, DropZone } from "react-native-drag-drop-and-swap"
+
+let alphaData = [];
+let first = "A", last = "E";
+for (var i = first.charCodeAt(0); i <= last.charCodeAt(0); i++) {
+  alphaData.push({ data: eval("String.fromCharCode(" + i + ")"), id: i });
+}
+
+export default class LaunchScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onDrop = this.onDrop.bind(this);
+    this.onHover = this.onHover.bind(this);
+    this.onHold = this.onHold.bind(this);
+
+    this.state = {
+      alphabets: alphaData,
+      hoverData: {},
+      dropData: {},
+      hoverDataIndex: null
+    };
+  }
+
+  onDrop(data, index) {
+    let alphabets = this.state.alphabets.map((item, i) => {
+      if (item.id == data.id) {
+        return this.state.hoverData;
+      }
+      if (item.id == this.state.hoverData.id) {
+        return data;
+      }
+      return item;
+    });
+    this.setState({ alphabets });
+  }
+
+  onHold(e) {
+    let data = this.state.alphabets || [];
+    let alphabets = data.filter((item, i) => e.id !== item.id)
+    if (this.state.hold && this.state.hold.id) {
+      alphabets.push(this.state.hold);
+    }
+    this.setState({ alphabets, hold: e })
+  }
+
+  onHover(hoverData, hoverDataIndex) {
+    this.setState({ hoverData, hoverDataIndex });
+  }
+
+  render() {
+    return (
+      <DragContainer>
+        <View style={{ flex: 1, justifyContent: "space-around", paddingTop:20 }}>
+          <View style={{ flex: 0.2 }}>
+            <View
+              style={{
+                flex: 1,
+                padding: 20,
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-around"
+              }}
+            >
+              <DropZone onDrop={this.onHold}>
+                <MyDropZoneContent hold={this.state.hold} />
+              </DropZone>
+              <DropZone onDrop={e => console.log("Dropped it on area 2", e)}>
+                <MyDropZoneContent />
+              </DropZone>
+            </View>
+          </View>
+
+          <View style={{ flex: 0.7, justifyContent: "center" }}>
+            <View
+              style={{
+                justifyContent: "center",
+                alignItems: "flex-end",
+                flexDirection: "row",
+                flexWrap: "wrap"
+              }}
+            >
+              {this.state.alphabets.map((item, i) => (
+                <Draggy
+                  key={i}
+                  alphabet={item}
+                  onHover={this.onHover}
+                  onDrop={this.onDrop}
+                  index={i}
+                />
+              ))}
+            </View>
+          </View>
+        </View>
+      </DragContainer>
+    );
+  }
+}
+
+class DraggyInner extends Component {
+  render() {
+    if (this.props.dragOver && !this.props.ghost && !this.props.dragging) {
+      LayoutAnimation.easeInEaseOut();
+      return (
+        <View
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            height: this.props.dragOver ? 40 : 36,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: "bold"
+            }}
+          >
+            {this.props.alphabet.data}
+          </Text>
+        </View>
+      );
+    }
+
+    let shadows = {
+      shadowColor: "black",
+      shadowOffset: { width: 0, height: 20 },
+      shadowOpacity: 0.5,
+      shadowRadius: 20,
+    };
+
+    return (
+      <View
+        style={[
+          {
+            height: 36,
+            width: 108,
+            backgroundColor: '#dddddd',
+            alignItems: "center",
+            justifyContent: "center",
+            paddingHorizontal: 4,
+            borderRadius: 4,
+          },
+          this.props.dragging ? shadows : null
+        ]}
+      >
+        <Text
+          style={{
+            fontSize: 14,
+            fontWeight: "bold"
+          }}
+        >
+          {this.props.alphabet.data}
+        </Text>
+      </View>
+    );
+  }
+}
+
+class Draggy extends Component {
+  render() {
+    return (
+      <Draggable data={this.props.alphabet} style={{ margin: 4 }}>
+        <DropZone
+          disabled
+          onDrop={e => this.props.onDrop(e, this.props.index)}
+          onEnter={e =>
+            this.props.onHover(this.props.alphabet, this.props.index)
+          }
+        >
+          <DraggyInner
+            alphabet={this.props.alphabet}
+            index={this.props.index}
+          />
+        </DropZone>
+      </Draggable>
+    );
+  }
+}
+
+class MyDropZoneContent extends Component {
+  componentWillReceiveProps({ dragOver }) {
+    if (dragOver !== this.props.dragOver) LayoutAnimation.easeInEaseOut();
+  }
+  render() {
+    return (
+      <View
+        style={{
+          width: this.props.dragOver ? 116 : 108,
+          height: this.props.dragOver ? 44 : 36,
+          backgroundColor: "#dddddd",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 4,
+        }}
+      >
+        <View>
+          <Text style={{fontWeight:"bold", fontSize: 14, fontWeight: 'bold'}}>{this.props.hold ? this.props.hold.data : "__________"}</Text>
+        </View>
+      </View>
+    );
+  }
+}
